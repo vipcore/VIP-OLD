4d9bf30971b39845a09348cd998dc1e9fb4090c8
vipcore <vipcore2018@gmail.com>
2020-06-29 07:38:12 +0700

    add auto install masternode script

---
+++ mnscriptinstall18.sh
@@ -0,0 +1,110 @@
+#/bin/bash
+cd ~
+echo "****************************************************************************"
+echo "* This script will install and configure your VIP Coin masternodes.       *"
+echo "*                                                                         *"
+echo "*    If you have any issues please ask for help on the VIP discord.       *"
+echo "*                      https://discord.gg/xKPXvyw                          *"
+echo "*                        https://vipcore.vip/                             *"
+echo "****************************************************************************"
+echo "" 
+echo ""
+echo ""
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo ""
+echo ""
+echo ""
+echo "Do you want to install all needed updates and firewall settings (no if you did it before)? [y/n]"
+read DOSETUP
+	if [[ $DOSETUP =~ "y" ]] || [[$DOSETUP =~ "Y" ]] ; then
+	sudo apt-get update
+	sudo apt-get -y upgrade
+	sudo apt-get -y dist-upgrade
+	
+	sudo apt-get install -y ufw
+	sudo ufw allow ssh/tcp
+	sudo ufw limit ssh/tcp
+	sudo ufw logging on
+	sudo ufw allow 22
+	echo "y" | sudo ufw enable
+	sudo ufw status
+fi
+
+echo ""
+wget https://github.com/vipcore/VIP/releases/download/V.1.0/linux-18-daemon.zip
+unzip linux-18-daemon.zip
+echo ""
+echo "Configure your masternodes now!"
+echo "Your recognised IP address is:"
+sudo hostname -I 
+echo "Is this the IP you wish to use for MasterNode ? [y/n] , followed by [ENTER]"
+read IPDEFAULT
+	if [[ $IPDEFAULT =~ "y" ]] || [[$IPDEFAULT =~ "Y" ]] ; then
+	echo ""
+	echo "We are using your default IP address"
+	echo "Enter masternode private key for node, followed by [ENTER]: $ALIAS"
+	read PRIVKEY
+	CONF_DIR=~/.vip\/
+	CONF_FILE=vip.conf
+	PORT=19450
+	IP=$(hostname -I)
+	mkdir -p $CONF_DIR
+	echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+	echo "rpcpassword=passw"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+	echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+	echo "listen=1" >> $CONF_DIR/$CONF_FILE
+	echo "server=1" >> $CONF_DIR/$CONF_FILE
+	echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+	echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+	echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
+	echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+	echo "" >> $CONF_DIR/$CONF_FILE
+	echo "" >> $CONF_DIR/$CONF_FILE
+	echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
+	echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+	echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+	./vipd -daemon
+	echo "if server start failure try ./vipd -reindex"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!                                                 !"
+	echo "! Your MasterNode Is setup please close terminal  !"
+	echo "!   and continue the local wallet setup guide     !"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo ""
+else	
+	echo "Type the custom IP of this node, followed by [ENTER]:"
+	read DIP
+	echo ""
+	echo "Enter masternode private key for node, followed by [ENTER]: $ALIAS"
+	read PRIVKEY
+	CONF_DIR=~/.vip\/
+	CONF_FILE=vip.conf
+	PORT=19450
+	mkdir -p $CONF_DIR
+	echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+	echo "rpcpassword=passw"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+	echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+	echo "listen=1" >> $CONF_DIR/$CONF_FILE
+	echo "server=1" >> $CONF_DIR/$CONF_FILE
+	echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+	echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+	echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
+	echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+	echo "" >> $CONF_DIR/$CONF_FILE
+	echo "" >> $CONF_DIR/$CONF_FILE
+	echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
+	echo "masternodeaddr=$DIP:$PORT" >> $CONF_DIR/$CONF_FILE
+	echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+	./vipd -daemon
+	echo "if server start failure try ./vipd -reindex"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo "!                                                 !"
+	echo "! Your MasterNode Is setup please close terminal  !"
+	echo "!   and continue the local wallet setup guide     !"
+	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	echo ""
+fi
